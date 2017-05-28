88651a076286dd26b658abfd51822a730465b583
Menzies <stefan_schenk@hotmail.com>
2017-05-28 15:55:29 +0200

    Added vue component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
+        
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
@@ -12,6 +13,9 @@
                 </div>
             </div>
         </div>
+
+        <Canvas></Canvas>
+
     </div>
 </div>
 @endsection
