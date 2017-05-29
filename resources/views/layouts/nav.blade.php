910466bdc39f27ff7774e2f8d3b3522d4709b0f3
Menzies <stefan_schenk@hotmail.com>
2017-05-29 20:55:27 +0200

    Improved routing

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -11,7 +11,7 @@
             </button>
 
             <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
+            <a class="navbar-brand" href="{{ url('/home') }}">
                 {{ config('app.name', 'Toxic') }}
             </a>
         </div>
@@ -26,9 +26,7 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    <li><a href="{{ route('login') }}">Login</a></li>
                     <li><a href="{{ route('login/facebook') }}">Register</a></li>
-                    {{-- <li><a href="{{ route('register') }}">Register</a></li> --}}
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,40 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+/*
+|--------------------------------------------------------------------------
+| Login Routes
+|--------------------------------------------------------------------------
+|
+| Auth routes are overridden by manual logout and login routes ensuring
+| that the default login and register page are not accessible, while
+| still keeping the same route names for the uncommenting of:
+|
+// Auth::routes();
+|
+*/
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['namespace' => 'Auth'], function() {
 
-Route::group(['prefix' => 'login'], function() {
-    Route::get('/facebook', 'LoginController@redirectToFacebook')->name('login/facebook');
-    Route::get('/facebook/callback', 'LoginController@handleFacebookCallback');
+    Route::post('/logout', 'LoginController@logout')->name('logout');
+    Route::get('/login', 'LoginController@redirectToFacebook')->name('login');
+    
+    Route::group(['prefix' => 'login'], function() {
+        Route::get('/facebook', 'LoginController@redirectToFacebook')->name('login/facebook');
+        Route::get('/facebook/callback', 'LoginController@handleFacebookCallback');
+    });
+});
+
+/*
+|--------------------------------------------------------------------------
+| User Routes
+|--------------------------------------------------------------------------
+|
+| After logging in, the authentication routes should be accessible for
+| registered users. 
+|
+*/
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
 });
