70244016eca0a812f5b84a7fe579f63d26fc97ff
STEFAN-DESKTOP\Stefan <stefan_schenk@hotmail.com>
2017-07-09 21:59:42 +0200

    Added meta tags for Link Previews

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, user-scalable=0, initial-scale=1.0, minimum-scale=1, maximum-scale=1">
 
+
     <!-- Tokens -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="api_token" content="{{ Helpers::api_token() }}" id="api_token">
@@ -12,9 +13,14 @@
 
     <!-- Title -->
     <title>{{ config('app.name', 'Toxic') }}</title>
+    <meta name="description" content="Censorship feeds the dirty mind more than the four-letter word itself." />
+    <meta property="og:description" content="Censorship feeds the dirty mind more than the four-letter word itself." />
+    <meta property="og:title" content="Combating Censorship" />
+    <meta property="og:image" content="" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('favicons/apple-touch-icon.png') }}" type="image/x-icon" />
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
